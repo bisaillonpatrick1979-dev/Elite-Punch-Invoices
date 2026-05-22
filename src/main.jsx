b1e26e877cf14d7112a4168efe1cbe348d302516
Hailite <bisaillonpatrick1979@gmail.com>
2026-05-22 13:46:44 -0600

    Wrap app with data provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { AppDataProvider } from "./context/AppDataContext.jsx";
 
 import "./styles/themes.css";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppDataProvider>
+      <App />
+    </AppDataProvider>
   </React.StrictMode>
 );
