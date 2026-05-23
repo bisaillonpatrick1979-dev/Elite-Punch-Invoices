83ab4308912a27cb10fba92949a3f6789debdb2b
Hailite <bisaillonpatrick1979@gmail.com>
2026-05-22 18:35:31 -0600

    Wrap app with error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import { AppDataProvider } from "./context/AppDataContext.jsx";
 
 import "./styles/themes.css";
@@ -12,8 +13,10 @@ import "./styles/status.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppDataProvider>
-      <App />
-    </AppDataProvider>
+    <ErrorBoundary>
+      <AppDataProvider>
+        <App />
+      </AppDataProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
