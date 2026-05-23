6063565a2ce5537d1075eeab8c42e58206ab8904
Hailite <bisaillonpatrick1979@gmail.com>
2026-05-22 18:49:21 -0600

    Add app version constants

---
+++ src/constants/version.js
@@ -0,0 +1,16 @@
+export const APP_VERSION = "0.2.0";
+
+export const MVP_STATUS = {
+  punch: "basic-working",
+  breaks: "basic-working",
+  calendar: "basic-working",
+  invoices: "basic-working",
+  pdf: "basic-working",
+  clients: "basic-working",
+  workers: "basic-working",
+  payroll: "basic-working",
+  catalog: "basic-working",
+  accounting: "basic-working",
+  backup: "basic-working",
+  pwa: "basic-working"
+};
